a935d6e233e9871baf76ada2a0e79ff6d3c6d782
Hector E. Gomez Morales <hectoregm@gmail.com>
2014-06-11 21:52:15 -0500

    Initial commit BullsEye.

---
+++ BullsEye/BullsEye/BullsEyeViewController.h
@@ -0,0 +1,13 @@
+//
+//  BullsEyeViewController.h
+//  BullsEye
+//
+//  Created by Hector Enrique Gomez Morales on 6/11/14.
+//  Copyright (c) 2014 Hector Enrique Gomez Morales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BullsEyeViewController : UIViewController
+
+@end
---
+++ BullsEye/BullsEye/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BullsEye
+//
+//  Created by Hector Enrique Gomez Morales on 6/11/14.
+//  Copyright (c) 2014 Hector Enrique Gomez Morales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BullsEyeAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BullsEyeAppDelegate class]));
+    }
+}
