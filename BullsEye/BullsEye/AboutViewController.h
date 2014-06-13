1ab02f7e5605a9dbdd8af52ea29c04add88aafd8
Hector E. Gomez Morales <hectoregm@gmail.com>
2014-06-12 21:08:12 -0500

    Finished section 06 - About Screen.

---
+++ BullsEye/BullsEye/AboutViewController.h
@@ -0,0 +1,15 @@
+//
+//  AboutViewController.h
+//  BullsEye
+//
+//  Created by Hector Enrique Gomez Morales on 6/12/14.
+//  Copyright (c) 2014 Hector Enrique Gomez Morales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+- (IBAction)close;
+
+@end
