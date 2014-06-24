04a11544422f7e9a38980252398d55e46663588d
Hector E. Gomez Morales <hectoregm@gmail.com>
2014-06-23 22:36:34 -0500

    Initial changes for AllListViewController

---
+++ Checklists/Checklists/AllListsViewController.h
@@ -0,0 +1,13 @@
+//
+//  AllListsViewController.h
+//  Checklists
+//
+//  Created by Hector Enrique Gomez Morales on 6/22/14.
+//  Copyright (c) 2014 Hector Enrique Gomez Morales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AllListsViewController : UITableViewController
+
+@end
--- Checklists/Checklists/ChecklistViewController.m
+++ Checklists/Checklists/ChecklistViewController.m
@@ -6,14 +6,14 @@
 //  Copyright (c) 2014 Hector Enrique Gomez Morales. All rights reserved.
 //
 
-#import "ChecklistsViewController.h"
+#import "ChecklistViewController.h"
 #import "ChecklistItem.h"
 
-@interface ChecklistsViewController ()
+@interface ChecklistViewController ()
 
 @end
 
-@implementation ChecklistsViewController
+@implementation ChecklistViewController
 {
     NSMutableArray *_items;
 }
