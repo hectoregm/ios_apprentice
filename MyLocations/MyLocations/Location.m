e6a0db079369f4a1cef59b50170e3dc2f2de8925
Hector E. Gomez Morales <hectoregm@gmail.com>
2014-06-30 19:59:03 -0500

    Finished part 4 from Tutorial 3.

---
+++ MyLocations/MyLocations/Location.m
@@ -0,0 +1,21 @@
+//
+//  Location.m
+//  MyLocations
+//
+//  Created by Hector Enrique Gomez Morales on 6/30/14.
+//  Copyright (c) 2014 Hector Enrique Gomez Morales. All rights reserved.
+//
+
+#import "Location.h"
+
+
+@implementation Location
+
+@dynamic latitude;
+@dynamic longitude;
+@dynamic date;
+@dynamic locationDescription;
+@dynamic category;
+@dynamic placemark;
+
+@end
--- MyLocations/MyLocations/LocationDetailsViewController.m
+++ MyLocations/MyLocations/LocationDetailsViewController.m
@@ -9,6 +9,7 @@
 #import "LocationDetailsViewController.h"
 #import "CategoryPickerViewController.h"
 #import "HudView.h"
+#import "Location.h"
 
 @interface LocationDetailsViewController () <UITextViewDelegate>
 
@@ -25,6 +26,7 @@
 {
     NSString *_descriptionText;
     NSString *_categoryName;
+    NSDate *_date;
 }
 
 - (id)initWithCoder:(NSCoder *)aDecoder
@@ -32,6 +34,7 @@
     if ((self = [super initWithCoder:aDecoder])) {
         _descriptionText = @"";
         _categoryName = @"No Category";
+        _date = [NSDate date];
     }
     
     return self;
@@ -52,7 +55,7 @@
         self.addressLabel.text = @"No Address Found";
     }
     
-    self.dateLabel.text = [self formatDate:[NSDate date]];
+    self.dateLabel.text = [self formatDate:_date];
     
     UITapGestureRecognizer *gestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(hideKeyboard:)];
     gestureRecognizer.cancelsTouchesInView = NO;
@@ -95,6 +98,22 @@
 {
     HudView *hudView = [HudView hudInView:self.navigationController.view animated:YES];
     hudView.text = @"Tagged";
+    
+    Location *location = [NSEntityDescription insertNewObjectForEntityForName:@"Location"
+                                                       inManagedObjectContext:self.managedObjectContext];
+    
+    location.locationDescription = _descriptionText;
+    location.category = _categoryName;
+    location.latitude = @(self.coordinate.latitude);
+    location.longitude = @(self.coordinate.longitude);
+    location.date = _date;
+    location.placemark = self.placemark;
+    
+    NSError *error;
+    if (![self.managedObjectContext save:&error]) {
+        FATAL_CORE_DATA_ERROR(error);
+        return;
+    }
     
     [self performSelector:@selector(closeScreen) withObject:nil afterDelay:0.6];
 }
