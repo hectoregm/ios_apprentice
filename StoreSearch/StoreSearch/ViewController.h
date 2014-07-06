b91eb430f09f6fb5936d9696c0a1038ca7992d63
Hector E. Gomez Morales <hectoregm@gmail.com>
2014-07-05 19:26:33 -0500

    Initial commit StoreSearch Tutorial.

---
+++ StoreSearch/StoreSearch/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  StoreSearch
+//
+//  Created by Hector Enrique Gomez Morales on 7/5/14.
+//  Copyright (c) 2014 Hector Enrique Gomez Morales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
