7ee299ec59b5c2b61b815798eb6627a9c3a42ea6
Hector E. Gomez Morales <hectoregm@gmail.com>
2014-12-26 07:58:57 -0800

    Initial checkin for Checklists

---
+++ Checklists/Checklists/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Hector Enrique Gomez Morales on 12/26/14.
+//  Copyright (c) 2014 Hector Enrique Gomez Morales. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
