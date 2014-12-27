90cd52c9562ac1cbc77f79686bbf54dfe08e3438
Hector E. Gomez Morales <hectoregm@gmail.com>
2014-12-27 06:04:35 -0800

    Add data model ChecklistItem.

---
+++ Checklists/Checklists/ChecklistItem.swift
@@ -0,0 +1,18 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Hector Enrique Gomez Morales on 12/27/14.
+//  Copyright (c) 2014 Hector Enrique Gomez Morales. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+    
+    func toggleChecked() {
+        checked = !checked
+    }
+}
