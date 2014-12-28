e28541e0ba9e227189fca1d6d6ddecee8c448dfa
Hector E. Gomez Morales <hectoregm@gmail.com>
2014-12-27 21:03:37 -0800

    Work in the Add Item screen

---
+++ Checklists/Checklists/AddItemViewController.swift
@@ -0,0 +1,46 @@
+//
+//  AddItemViewController.swift
+//  Checklists
+//
+//  Created by Hector Enrique Gomez Morales on 12/27/14.
+//  Copyright (c) 2014 Hector Enrique Gomez Morales. All rights reserved.
+//
+
+import UIKit
+
+class AddItemViewController: UITableViewController , UITextFieldDelegate {
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.rowHeight = 44
+    }
+    
+    override func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
+        return nil
+    }
+    
+    @IBAction func cancel() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func done() {
+        println("Contents of the text field: \(textField.text)")
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        let oldText: NSString = textField.text
+        let newText: NSString = oldText.stringByReplacingCharactersInRange(range, withString: string)
+        
+        doneBarButton.enabled = (newText.length > 0)
+
+        return true
+    }
+}
