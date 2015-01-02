d4fef90172680d0adeda8e3bd8e6803c19852019
Hector E. Gomez Morales <hectoregm@gmail.com>
2015-01-01 22:10:47 -0800

    Add selection of icon for check lists

--- Checklists/Checklists/Checklist.swift
+++ Checklists/Checklists/Checklist.swift
@@ -10,22 +10,30 @@ import UIKit
 
 class Checklist: NSObject, NSCoding {
     var name = ""
+    var iconName: String
     var items = [ChecklistItem]()
     
     required init(coder aDecoder: NSCoder) {
         name = aDecoder.decodeObjectForKey("Name") as String
         items = aDecoder.decodeObjectForKey("Items") as [ChecklistItem]
+        iconName = aDecoder.decodeObjectForKey("IconName") as String
         super.init()
     }
     
-    init(name: String) {
+    convenience init(name: String) {
+        self.init(name: name, iconName: "No Icon")
+    }
+    
+    init(name: String, iconName: String) {
         self.name = name
+        self.iconName = iconName
         super.init()
     }
     
     func encodeWithCoder(aCoder: NSCoder) {
         aCoder.encodeObject(name, forKey: "Name")
         aCoder.encodeObject(items, forKey: "Items")
+        aCoder.encodeObject(iconName, forKey: "IconName")
     }
     
     func countUncheckedItems() -> Int {
