3d7b905a7302c724f1fffec7837f989c83160003
Hector E. Gomez Morales <hectoregm@gmail.com>
2015-01-04 19:07:05 -0800

    Initial commit for MyLocations

---
+++ MyLocations/MyLocations/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MyLocations
+//
+//  Created by Hector Enrique Gomez Morales on 1/4/15.
+//  Copyright (c) 2015 Hector Enrique Gomez Morales. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
