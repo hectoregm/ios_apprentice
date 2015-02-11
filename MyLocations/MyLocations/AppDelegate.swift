bccc5a24fc43998f5997d14ecfd769f27baeeb56
Hector E. Gomez Morales <hectoregm@gmail.com>
2015-02-10 22:26:13 -0600

    First changes to apperance using UIKit appearance calls.

--- MyLocations/MyLocations/AppDelegate.swift
+++ MyLocations/MyLocations/AppDelegate.swift
@@ -18,6 +18,14 @@ func fatalCoreDataError(error: NSError?) {
     }
 }
 
+func customizeAppearance() {
+    UINavigationBar.appearance().barTintColor = UIColor.blackColor()
+    UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
+    UITabBar.appearance().barTintColor = UIColor.blackColor()
+    let tintColor = UIColor(red: 255/255.0, green: 238/255.0, blue: 136/255.0, alpha: 1.0)
+    UITabBar.appearance().tintColor = tintColor
+}
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -51,6 +59,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        customizeAppearance()
         let tabBarController = window!.rootViewController as UITabBarController
         
         if let tabBarViewControllers = tabBarController.viewControllers {
