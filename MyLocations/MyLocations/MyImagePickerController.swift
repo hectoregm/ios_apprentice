782c938ad87086a90a7bff3458c6ab6e30f5176b
Hector E. Gomez Morales <hectoregm@gmail.com>
2015-02-11 21:31:04 -0600

    Final changes to have always a white status bar.

---
+++ MyLocations/MyLocations/MyImagePickerController.swift
@@ -0,0 +1,15 @@
+//
+//  MyImagePickerController.swift
+//  MyLocations
+//
+//  Created by Hector Enrique Gomez Morales on 2/11/15.
+//  Copyright (c) 2015 Hector Enrique Gomez Morales. All rights reserved.
+//
+
+import UIKit
+
+class MyImagePickerController: UIImagePickerController {
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .LightContent
+    }
+}
---
+++ MyLocations/MyLocations/MyNavigationController.swift
@@ -0,0 +1,15 @@
+//
+//  MyNavigationController.swift
+//  MyLocations
+//
+//  Created by Hector Enrique Gomez Morales on 2/11/15.
+//  Copyright (c) 2015 Hector Enrique Gomez Morales. All rights reserved.
+//
+
+import UIKit
+
+class MyNavigationController: UINavigationController {
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .LightContent
+    }
+}
