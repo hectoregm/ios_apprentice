e5a2f261a8f0fd77dda56dab4efb717c1b642095
Hector E. Gomez Morales <hectoregm@gmail.com>
2015-02-12 19:39:31 -0600

    Fix colors for category picker.

--- MyLocations/MyLocations/CategoryPickerViewController.swift
+++ MyLocations/MyLocations/CategoryPickerViewController.swift
@@ -28,6 +28,9 @@ class CategoryPickerViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.backgroundColor = UIColor.blackColor()
+        tableView.separatorColor = UIColor(white: 1.0, alpha: 0.2)
+        tableView.indicatorStyle = .White
     }
     
     // MARK: - UITableViewDataSource
@@ -68,6 +71,19 @@ class CategoryPickerViewController: UITableViewController {
             selectedIndexPath = indexPath
             println("In didSelectRow")
         }
+    }
+    
+    override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        cell.backgroundColor = UIColor.blackColor()
+        
+        if let textLabel = cell.textLabel {
+            textLabel.textColor = UIColor.whiteColor()
+            textLabel.highlightedTextColor = textLabel.textColor
+        }
+        
+        let selectionView = UIView(frame: CGRect.zeroRect)
+        selectionView.backgroundColor = UIColor(white: 1.0, alpha: 0.2)
+        cell.selectedBackgroundView = selectionView
     }
 
     // MARK: - Navigation
